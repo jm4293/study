b363b7ec6db421719534582a7add801bdfd338cd
jm4293 <dlwoals4293@gmail.com>
2024-10-13 01:42:55 +0900

    [Docs]: 2주차 backend 코드

--- 2-week/backend/index.js
+++ 2-week/backend/index.js
@@ -1,23 +1,30 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-// app.use(express.static(path.join(__dirname, "./public")));
-
+//
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/study/:page", (req, res) => {
+app.get("/study", (req, res) => {
   console.log(req.params);
   console.log(req.query);
   res.send("study");
 });
 
+app.use(express.static(path.join(__dirname, "./public")));
+
 app.get("/html", (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.use(express.static(path.join(__dirname, "./public/build")));
+
+app.get("/react", (req, res) => {
+  res.sendFile(`${__dirname}/public/build/index.html`);
 });
 
 app.listen(port, () => {
