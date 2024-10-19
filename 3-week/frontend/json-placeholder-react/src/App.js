c4dc9931cbe092107dcb60933c9fae8293ab1bb9
jm4293 <dlwoals4293@gmail.com>
2024-10-20 00:53:59 +0900

    [Fix]: BrowserRouter as Router을 BrowserRouter로 변경

--- 3-week/frontend/json-placeholder-react/src/App.js
+++ 3-week/frontend/json-placeholder-react/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BoardList from "./pages/board-list/BoardList";
 import BoardDetail from "./pages/board-detail/BoardDetail";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<BoardList />} />
         <Route path="/detail/:id" element={<BoardDetail />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
